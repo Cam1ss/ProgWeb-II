f3d90c901f433ea51d86e9f7906932a89a312007
Camila Braúna <125037138+Cam1ss@users.noreply.github.com>
2024-10-22 12:15:37 -0300

    Add files via upload

---
+++ formulario/js.js
@@ -0,0 +1,8 @@
+
+var filtro = function(event){
+    return ((event.charCode >=48 && event.charCode <=57) || (event.keyCode==45) || (event.charCode >=46))
+} 
+// função para impedir de ser inserido letras
+
+
+
